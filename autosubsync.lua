2e12c5fe4c875ae7251b69de81e75b1cbefa2ee9
Ren Tatsumoto <tatsu@autistici.org>
2020-09-22 02:25:16 +0300

    reformat file

--- autosubsync.lua
+++ autosubsync.lua
@@ -7,67 +7,73 @@ local utils = require 'mp.utils'
 -- originally found at: https://stackoverflow.com/a/30960054
 local binary_format = package.cpath:match("%p[\\|/]?%p(%a+)")
 if binary_format == "dll" then
-  function os.name()
-    return "Windows"
-  end
+    function os.name()
+        return "Windows"
+    end
 elseif binary_format == "so" then
-  function os.name()
-    return "Linux"
-  end
+    function os.name()
+        return "Linux"
+    end
 elseif binary_format == "dylib" then
-  function os.name()
-    return "macOS"
-  end
+    function os.name()
+        return "macOS"
+    end
 end
 binary_format = nil
 
 -- Chooses the default location of the ffsubsync executable depending on the operating system
 if os.name() == "Linux" or os.name() == "macOS" then
-    default_subsync_location = utils.join_path(os.getenv("HOME"), ".local/bin/ffsubsync") 
-elseif os.name() == "Windows" then 
+    default_subsync_location = utils.join_path(os.getenv("HOME"), ".local/bin/ffsubsync")
+elseif os.name() == "Windows" then
     default_subsync_location = "%APPDATA%/Python/Scripts/ffsubsync"
 end
 
 function display_error()
-  mp.msg.warn("Subtitle synchronization failed: ")
-  mp.osd_message("Subtitle synchronization failed")
+    mp.msg.warn("Subtitle synchronization failed: ")
+    mp.osd_message("Subtitle synchronization failed")
 end
 
 -- Courtesy of https://stackoverflow.com/questions/4990990/check-if-a-file-exists-with-lua
 function file_exists(filepath)
-   local f=io.open(filepath,"r")
-   if f~=nil then io.close(f) return true else return false end
+    local f = io.open(filepath, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
 end
 
 function sync_sub_fn()
-  path = mp.get_property("path")
-  srt_path = string.gsub(path, "%.%w+$", ".srt")
-  if file_exists(srt_path)==false then
-    mp.msg.warn("Couldn't find",srt_path)
-    display_error()
-    do return end
-  end 
-
-  -- Replace the following line if the location of ffsubsync differs from the defaults
-  -- You may use 'which ffsubsync' to find the path
-  subsync = default_subsync_location
-  t = {}
-  t.args = {subsync, path, "-i",srt_path,"-o",srt_path}
-
-  mp.osd_message("Sync subtitle...")
-  mp.msg.info("Starting ffsubsync...")
-  res = utils.subprocess(t)
-  if res.error == nil then
-    if mp.commandv("sub_add", srt_path) then
-      mp.msg.info("Subtitle updated")
-      mp.osd_message("Subtitle at'" .. srt_path .. "' synchronized")
-    else
-      display_error()
+    path = mp.get_property("path")
+    srt_path = string.gsub(path, "%.%w+$", ".srt")
+    if file_exists(srt_path) == false then
+        mp.msg.warn("Couldn't find", srt_path)
+        display_error()
+        do
+            return
+        end
     end
-  else
-    display_error()
-  end
+
+    -- Replace the following line if the location of ffsubsync differs from the defaults
+    -- You may use 'which ffsubsync' to find the path
+    subsync = default_subsync_location
+    t = {}
+    t.args = { subsync, path, "-i", srt_path, "-o", srt_path }
+
+    mp.osd_message("Sync subtitle...")
+    mp.msg.info("Starting ffsubsync...")
+    res = utils.subprocess(t)
+    if res.error == nil then
+        if mp.commandv("sub_add", srt_path) then
+            mp.msg.info("Subtitle updated")
+            mp.osd_message("Subtitle at'" .. srt_path .. "' synchronized")
+        else
+            display_error()
+        end
+    else
+        display_error()
+    end
 end
 
 mp.add_key_binding("n", "auto_sync_subs", sync_sub_fn)
-
